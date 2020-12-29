8007499b11fed254f3637a54d676c29f99b0693f
vuongvgc <vuongvgc@gmail.com>
2020-12-29 20:51:04 +0700

    season-app: reuse login

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 export function App() {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState("");
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => setLat(position.coords.latitude),
-      (err) => setErrorMessage(err.message)
-    );
-  }, []);
+  // const [lat, setLat] = useState(null);
+  // const [errorMessage, setErrorMessage] = useState("");
+  // useEffect(() => {
+  //   window.navigator.geolocation.getCurrentPosition(
+  //     (position) => setLat(position.coords.latitude),
+  //     (err) => setErrorMessage(err.message)
+  //   );
+  // }, []);
+  const [lat, errorMessage] = useLocation();
   let renderContent = () => {
     if (errorMessage) {
       return <div>Error: {errorMessage}</div>;
